d6d551081cda6d2aff4302adefac33df8ba1fd13
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-05-22 00:13:14 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId);
 }
 
-function getCommentById(commentId) {
+function getCommentsByPostId(commentId) {
   return commentsFromServer.filter(comment => comment.postId === commentId);
 }
 
 export const posts = postsFromServer.map(post => ({
   ...post,
   user: getUserById(post.userId),
-  comments: getCommentById(post.id),
+  comments: getCommentsByPostId(post.id),
 }));
 
 export const App = () => (
